436016c90a3f88580b5be2ae63ee843efa8ef7e2
wnderbin <->
2025-05-30 10:26:50 +0900

    -

--- handlers/vpn.go
+++ handlers/vpn.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -30,11 +31,23 @@ func (h *TelegramHandler) HandleGenerateVPNConfig(c telebot.Context) error {
 	}
 	privateKeyStr := strings.TrimSpace(string(privateKey))
 
-	publicKey, err := exec.Command("wg", "pubkey", privateKeyStr).Output()
+	/*publicKey, err := exec.Command("wg", "pubkey", privateKeyStr).Output()
 	if err != nil {
 		h.Logger.Printf("[ ERROR ] public key generation error: %s\n", err)
 		return c.Send("❌ Ошибка генерации публичного ключа")
+	}*/
+	cmd := exec.Command("wg", "pubkey")
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		h.Logger.Printf("[ ERROR ] creating stdin pipe: %s\n", err)
+		return c.Send("❌ Ошибка создания потока данных")
 	}
+
+	go func() {
+		defer stdin.Close()
+		io.WriteString(stdin, privateKeyStr)
+	}()
+	publicKey, err := cmd.Output()
 	publicKeyStr := strings.TrimSpace(string(publicKey))
 
 	clientIP := fmt.Sprintf("10.8.0.%d", (user.ID%253)+2)
@@ -64,11 +77,11 @@ func (h *TelegramHandler) HandleGenerateVPNConfig(c telebot.Context) error {
 		return c.Send("❌ Ошибка сохранения конфигурации")
 	}
 
-	cmd := exec.Command("wg", "set", conf.WGInterface,
+	cmd1 := exec.Command("wg", "set", conf.WGInterface,
 		"peer", publicKeyStr,
 		"allowed-ips", clientIP+"/32")
 
-	if output, err := cmd.CombinedOutput(); err != nil {
+	if output, err := cmd1.CombinedOutput(); err != nil {
 		h.Logger.Printf("[ ERROR ] adding client error: %s, output: %s", err, string(output))
 		return c.Send("❌ Ошибка добавления клиента в сервер")
 	}
