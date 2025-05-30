fdc2cd1315081e60e00ee05f31588a60423b3713
wnderbin <->
2025-05-30 09:45:08 +0900

    -

--- init-server/init.py
+++ init-server/init.py
@@ -1,36 +1,6 @@
 import os
 import subprocess
 
-def install_wireguard():
-    print('\t-- Установка wireguard')
-    subprocess.run(['apt-get', 'install', '-y', 'wireguard-tools', 'resolvconf'])
-
-def install_dependecies():
-    print('-- Установка зависимостей...')
-
-    print('\t-- Обновление списка доступных репозиториев')
-    subprocess.run(['apt', 'update', '-y'])
-
-    print('\t-- Обновление установленных пакетов до последних стабильных версий')
-    subprocess.run(['apt', 'upgrade', '-y'])
-
-    install_wireguard()
-
-    print('-- Установка зависимостей завершена')
-
-def generate_keys():
-    print('-- Генерация ключей wireguard...')
-    print('\t-- Создание директорий')
-    os.makedirs('/etc/wireguard', exist_ok=True)
-
-    subprocess.run(['wg', 'genkey', '|','tee', '/etc/wireguard/privatekey'])
-    privkey = subprocess.run(['cat', '/etc/wireguard/privatekey'], capture_output=True, text=True).stdout.split()
-    subprocess.run(['wg', 'pubkey', f'{privkey}', '|', 'tee', '/etc/wireguard/publickey'])
-    pubkey = subprocess.run(['cat', '/etc/wireguard/publickey'])
-    
-    print('-- Генерация ключей завершена')
-    return privkey, pubkey
-
 def create_wg0_config(privKey):
     
     print('-- Создание конфигурации wg0...')
@@ -58,13 +28,12 @@ def start_wireguard():
     print('-- Запуск завершен')
 
 def main():
-    install_dependecies()
-    privkey, pubkey = generate_keys()
+    privkey = input('Enter private key: ')
     create_wg0_config(privkey)
     ip_forwarding()
     start_wireguard()
 
-    print(f'\n[*] Инициализация завершена\nПриватный ключ: {privkey}\nПубличный ключ: {pubkey}\n --- НЕ ЗАБУДЬТЕ --- \n  1. Написать кофигурацию бота (config/config.yaml), указать IP-адрес и токен бота.\n')
+    print(f'\n[*] Инициализация завершена\n --- НЕ ЗАБУДЬТЕ --- \n  1. Написать кофигурацию бота (config/config.yaml), указать IP-адрес и токен бота.\n')
 
 if __name__ == '__main__':
     main()
